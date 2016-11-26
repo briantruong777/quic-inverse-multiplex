6b724213877bf78ac0e58e7b9ec66c8a57665531
Jing <jennyhuangjing@hotmail.com>
2016-11-25 22:05:14 -0600

    Add basic inverse-multiplexing client without multithreading.

---
+++ src/net/tools/quic/quic_inverse_multiplexing_client.cc
@@ -0,0 +1,200 @@
+#include "net/tools/quic/quic_inverse_multiplexing_client.h"
+
+#include <utility>
+
+#include "base/bind.h"
+#include "base/bind_helpers.h"
+#include "base/logging.h"
+#include "base/memory/ptr_util.h"
+#include "base/run_loop.h"
+#include "base/threading/thread.h"
+#include "base/threading/thread_task_runner_handle.h"
+#include "net/base/net_errors.h"
+#include "net/http/http_request_info.h"
+#include "net/http/http_response_info.h"
+#include "net/log/net_log_source.h"
+#include "net/log/net_log_with_source.h"
+#include "net/quic/chromium/quic_chromium_alarm_factory.h"
+#include "net/quic/chromium/quic_chromium_connection_helper.h"
+#include "net/quic/chromium/quic_chromium_packet_reader.h"
+#include "net/quic/chromium/quic_chromium_packet_writer.h"
+#include "net/quic/core/crypto/quic_random.h"
+#include "net/quic/core/quic_connection.h"
+#include "net/quic/core/quic_flags.h"
+#include "net/quic/core/quic_protocol.h"
+#include "net/quic/core/quic_server_id.h"
+#include "net/quic/core/spdy_utils.h"
+#include "net/spdy/spdy_header_block.h"
+#include "net/spdy/spdy_http_utils.h"
+#include "net/udp/udp_client_socket.h"
+
+using std::string;
+using std::unique_ptr;
+using std::vector;
+using base::MakeUnique;
+using base::StringPiece;
+
+namespace net {
+
+QuicInverseMultiplexingClient::QuicInverseMultiplexingClient(
+    IPEndPoint server_address,
+    const QuicServerId& server_id,
+    const QuicVersionVector& supported_versions,
+    unique_ptr<ProofVerifier> proof_verifier)
+    : QuicInverseMultiplexingClient(server_address,
+                                    server_id,
+                                    supported_versions,
+                                    QuicConfig(),
+                                    std::move(proof_verifier)) {}
+
+QuicInverseMultiplexingClient::QuicInverseMultiplexingClient(
+    IPEndPoint server_address,
+    const QuicServerId& server_id,
+    const QuicVersionVector& supported_versions,
+    const QuicConfig& config,
+    unique_ptr<ProofVerifier> proof_verifier)
+    : QuicClientBase(server_id,
+                     supported_versions,
+                     config,
+                     CreateQuicConnectionHelper(),
+                     CreateQuicAlarmFactory(),
+                     std::move(proof_verifier)) {
+  set_server_address(server_address);  // This sets server_address_. Keeps here
+                                       // so that other functions won't break.
+  server_addresses_.push_back(server_address);
+}
+
+QuicInverseMultiplexingClient::~QuicInverseMultiplexingClient() {
+  for (auto& client : clients_) {
+    if (client->connected()) {
+      client->session()->connection()->CloseConnection(
+          QUIC_PEER_GOING_AWAY, "Shutting down",
+          ConnectionCloseBehavior::SEND_CONNECTION_CLOSE_PACKET);
+    }
+  }
+}
+
+void QuicInverseMultiplexingClient::AddServerAddresses(
+                                    std::vector<IPEndPoint> server_addresses) {
+  server_addresses_.insert(server_addresses_.end(), server_addresses.begin(),
+                           server_addresses.end());
+}
+
+bool QuicInverseMultiplexingClient::Initialize() {
+  bool all_initialized = true;
+  for (auto& server_address : server_addresses_) {
+    cert_verifiers_.push_back(CertVerifier::CreateDefault());
+    unique_ptr<ProofVerifier> verifier;
+    if (proof_verifier() == nullptr) {
+      verifier = MakeUnique<FakeProofVerifier>();
+    } else {
+      verifier = MakeUnique<ProofVerifierChromium>(
+              cert_verifiers_.back().get(), new CTPolicyEnforcer(),
+              new TransportSecurityState, new MultiLogCTVerifier());
+    }
+    clients_.push_back(MakeUnique<QuicSimpleClient>(server_address, server_id(),
+                                                    supported_versions(),
+                                                    std::move(verifier)));
+    all_initialized &= clients_.back()->Initialize();
+  }
+  return all_initialized;
+}
+
+bool QuicInverseMultiplexingClient::Connect() {
+  // set_initial_max_packet_length need to be called before Connect().
+  bool is_connected = false;
+  for (auto& client : clients_) {
+    client->set_initial_max_packet_length(initial_max_packet_length());
+    if (client->Connect()) {
+      is_connected = true;
+    } else {
+      LOG(ERROR) << client->server_address().ToString()
+                 << " failed to connect.";
+    }
+  }
+  return is_connected;
+}   
+
+void QuicInverseMultiplexingClient::SendRequestAndWaitForResponse(
+      const SpdyHeaderBlock& headers, base::StringPiece body, bool fin) {
+  // Sends the same request through all the clients_.
+  // TODO: Use a thread for each client.
+  vector<unique_ptr<base::Thread>> threads;
+  for (auto& client : clients_) {
+    client->set_store_response(store_response_);
+    client->SendRequestAndWaitForResponse(headers, body, fin);
+  }
+  // TODO: parse the body of each response and buffer it in the right order.
+  // TODO: merge two loops together.
+  for (auto& client : clients_) {
+    latest_response_body_ += client->latest_response_body();
+  }
+  latest_response_headers_ = clients_.back()->latest_response_headers();
+  latest_response_trailers_ = clients_.back()->latest_response_trailers();
+  latest_response_code_ = clients_.back()->latest_response_code();
+}
+
+QuicAsyncStatus QuicInverseMultiplexingClient::FakeProofVerifier::VerifyProof(
+    const string& hostname,
+    const uint16_t port,
+    const string& server_config,
+    QuicVersion quic_version,
+    base::StringPiece chlo_hash,
+    const vector<std::string>& certs,
+    const string& cert_sct,
+    const string& signature,
+    const ProofVerifyContext* context,
+    string* error_details,
+    unique_ptr<ProofVerifyDetails>* details,
+    unique_ptr<ProofVerifierCallback> callback) {
+  return QUIC_SUCCESS;
+}
+
+QuicAsyncStatus
+QuicInverseMultiplexingClient::FakeProofVerifier::VerifyCertChain(
+    const string& hostname,
+    const vector<std::string>& certs,
+    const ProofVerifyContext* verify_context,
+    string* error_details,
+    unique_ptr<ProofVerifyDetails>* verify_details,
+    unique_ptr<ProofVerifierCallback> callback) {
+  return QUIC_SUCCESS;
+}
+
+
+// This is a stub.
+QuicChromiumConnectionHelper*
+QuicInverseMultiplexingClient::CreateQuicConnectionHelper() {
+  return new QuicChromiumConnectionHelper(&clock_, QuicRandom::GetInstance());
+}
+
+// This is a stub.
+QuicChromiumAlarmFactory*
+QuicInverseMultiplexingClient::CreateQuicAlarmFactory() {
+  return new QuicChromiumAlarmFactory(base::ThreadTaskRunnerHandle::Get().get(),
+                                      &clock_);
+}
+
+// This is a stub.
+IPEndPoint QuicInverseMultiplexingClient::GetLatestClientAddress() const {
+  return clients_.front()->GetLatestClientAddress();
+}
+
+// This is a stub.
+QuicPacketWriter* QuicInverseMultiplexingClient::CreateQuicPacketWriter() {
+  return nullptr;
+}
+
+// This is a stub.
+void QuicInverseMultiplexingClient::RunEventLoop() {}
+
+// This is a stub.
+bool QuicInverseMultiplexingClient::CreateUDPSocketAndBind(
+    IPEndPoint server_address, IPAddress bind_to_address, int bind_to_port) {
+  return true;
+}
+
+// This is a stub.
+void QuicInverseMultiplexingClient::CleanUpAllUDPSockets() {}
+
+}  // namespace net
---
+++ src/net/tools/quic/quic_inverse_multiplexing_client.h
@@ -0,0 +1,142 @@
+// An inverse multiplexing quic client.
+#ifndef NET_TOOLS_QUIC_QUIC_INVERSE_MULTIPLEXING_CLIENT_H_
+#define NET_TOOLS_QUIC_QUIC_INVERSE_MULTIPLEXING_CLIENT_H_
+
+#include <stddef.h>
+
+#include <memory>
+#include <string>
+
+#include "base/command_line.h"
+#include "base/macros.h"
+#include "base/strings/string_piece.h"
+#include "net/base/ip_address.h"
+#include "net/base/ip_endpoint.h"
+#include "net/http/http_response_headers.h"
+#include "net/log/net_log.h"
+#include "net/quic/chromium/quic_chromium_packet_reader.h"
+#include "net/quic/core/quic_config.h"
+#include "net/quic/core/quic_spdy_stream.h"
+#include "net/tools/quic/quic_client_base.h"
+#include "net/tools/quic/quic_simple_client.h"
+
+#include "net/cert/cert_verifier.h"
+#include "net/cert/multi_log_ct_verifier.h"
+#include "net/http/transport_security_state.h"
+#include "net/quic/chromium/crypto/proof_verifier_chromium.h"
+
+
+namespace net {
+
+class QuicInverseMultiplexingClient : public QuicClientBase {
+ public:
+  class FakeProofVerifier : public ProofVerifier {
+   public:
+    QuicAsyncStatus VerifyProof(
+        const std::string& hostname,
+        const uint16_t port,
+        const std::string& server_config,
+        QuicVersion quic_version,
+        base::StringPiece chlo_hash,
+        const std::vector<std::string>& certs,
+        const std::string& cert_sct,
+        const std::string& signature,
+        const ProofVerifyContext* context,
+        std::string* error_details,
+        std::unique_ptr<ProofVerifyDetails>* details,
+        std::unique_ptr<ProofVerifierCallback> callback) override;
+  
+    QuicAsyncStatus VerifyCertChain(
+        const std::string& hostname,
+        const std::vector<std::string>& certs,
+        const ProofVerifyContext* verify_context,
+        std::string* error_details,
+        std::unique_ptr<ProofVerifyDetails>* verify_details,
+        std::unique_ptr<ProofVerifierCallback> callback) override;
+  };
+
+  QuicInverseMultiplexingClient(IPEndPoint server_address,
+                                const QuicServerId& server_id,
+                                const QuicVersionVector& supported_versions,
+                                std::unique_ptr<ProofVerifier> proof_verifier);
+  QuicInverseMultiplexingClient(IPEndPoint server_address,
+                                const QuicServerId& server_id,
+                                const QuicVersionVector& supported_versions,
+                                const QuicConfig& config,
+                                std::unique_ptr<ProofVerifier> proof_verifier);
+
+  ~QuicInverseMultiplexingClient() override;
+
+  // Adds server addresses for multiple QUIC connections.
+  // This should be called before Initialize().
+  void AddServerAddresses(std::vector<IPEndPoint> server_addresses);
+  
+  // Initialize one or multiple QUIC simple clients with each connects to a
+  // given server_address. 
+  bool Initialize() override;
+
+  // TODO: Make the base class method virtual?
+  bool Connect();
+
+  void SendRequestAndWaitForResponse(const SpdyHeaderBlock& headers,
+                                     base::StringPiece body,
+                                     bool fin);
+
+  // This is a stub. It is only called in MigrateSocket().
+  IPEndPoint GetLatestClientAddress() const override;
+
+  // TODO: session()->error(). Session error handling.
+  // Get response.
+  const std::string& latest_response_headers() const { 
+    return latest_response_headers_; }
+  const std::string& latest_response_body() const { 
+    return latest_response_body_; }
+  const std::string& latest_response_trailers() const { 
+    return latest_response_trailers_; }
+  size_t latest_response_code() const { 
+    return latest_response_code_; }
+
+ protected:
+  // This is a stub.
+  QuicPacketWriter* CreateQuicPacketWriter() override;
+  // This is a stub.
+  void RunEventLoop() override;
+  // This is a stub.
+  bool CreateUDPSocketAndBind(IPEndPoint server_address,
+                              IPAddress bind_to_address,
+                              int bind_to_port) override;
+  // This is a stub.
+  void CleanUpAllUDPSockets() override;
+
+ private:
+  QuicChromiumConnectionHelper* CreateQuicConnectionHelper();
+  QuicChromiumAlarmFactory* CreateQuicAlarmFactory();
+
+  // Flag for storing response.
+  bool store_response_ = true;
+
+  // TODO: Remove this clock.
+  QuicClock clock_;
+  
+  // Server addresses.
+  std::vector<IPEndPoint> server_addresses_;
+
+  // Mutiple QuicSimpleClient.
+  std::vector<std::unique_ptr<QuicSimpleClient>> clients_;
+
+  // Certificate verifiers.
+  std::vector<std::unique_ptr<CertVerifier>> cert_verifiers_;
+
+  // Response.
+  std::string latest_response_headers_ = "";
+  std::string latest_response_body_ = "";
+  std::string latest_response_trailers_ = "";
+  size_t latest_response_code_ = 0;
+
+  DISALLOW_COPY_AND_ASSIGN(QuicInverseMultiplexingClient);
+};
+
+}  // namespace net
+
+#endif  // NET_TOOLS_QUIC_QUIC_INVERSE_MULTIPLEXING_CLIENT_H_
+
